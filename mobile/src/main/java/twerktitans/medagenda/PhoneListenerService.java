a504089e452361927b332eed847dc685440de588
Jeffrey Tan <jefftan274@gmail.com>
2016-04-18 17:47:53 -0700

    got rid of prog2-b skeleton code and put in working communication code between watch to phone

--- mobile/src/main/java/twerktitans/medagenda/PhoneListenerService.java
+++ mobile/src/main/java/twerktitans/medagenda/PhoneListenerService.java
@@ -2,9 +2,10 @@ package twerktitans.medagenda;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.Toast;
+
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
+
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -17,22 +18,17 @@ import java.nio.charset.StandardCharsets;
 public class PhoneListenerService extends WearableListenerService {
 
     //   WearableListenerServices don't need an iBinder or an onStartCommand: they just need an onMessageReceieved.
-    private static final String TOAST = "/send_toast";
+//    private static final String TOAST = "/send_toast";
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         Log.d("T", "in PhoneListenerService, got: " + messageEvent.getPath());
-        if( messageEvent.getPath().equalsIgnoreCase(TOAST) ) {
+        if( messageEvent.getPath().length() > 0 ) {
 
             // Value contains the String we sent over in WatchToPhoneService, "good job"
             String value = new String(messageEvent.getData(), StandardCharsets.UTF_8);
-
             // Make a toast with the String
             Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, value, duration);
-            toast.show();
 
             // so you may notice this crashes the phone because it's
             //''sending message to a Handler on a dead thread''... that's okay. but don't do this.
